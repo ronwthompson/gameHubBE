8a0efd1387cd9d6c31f3d3e68ffd457963f6ea00
CMunkyDev <matt@jcmueller.com>
2018-01-01 19:33:28 -0800

    change api call to be a post request so we can send a body more easily

--- src/routes/services.js
+++ src/routes/services.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const callHandler = require('../service-calls/services')
 
-router.get('/:service_name/:request_name', callHandler.shapeRequest)
+router.post('/:service_name/:request_name', callHandler.shapeRequest)
 
 module.exports = router
