facf7df0d1dc03d9891831fe283dad97fab8c971
CMunkyDev <matt@jcmueller.com>
2018-01-04 22:05:17 -0800

    add route to get all user steam info

--- src/service-calls/services.js
+++ src/service-calls/services.js
@@ -1,9 +1,37 @@
 const request = require('request')
+const rp = require('request-promise-native')
+const _ = require('lodash')
 
 const services = {
     steam: require('./steam')
 }
 
+function allUserInfo (req, res, next) {
+    let responseObject = {}
+    let promiseList = [
+        rp(services.steam.getplayersummaries({steamids: req.body.steamid})).json(),
+        rp(services.steam.getownedgames({steamid: req.body.steamid, include_played_free_games: req.body.include_played_free_games || 1})).json(),
+        rp(services.steam.getfriendlist({steamid: req.body.steamid})).json(),
+        rp(services.steam.getrecentlyplayedgames({steamid: req.body.steamid})).json(),
+        rp(services.steam.getplayerbans({steamids: req.body.steamid})).json()
+    ]
+   Promise.all(promiseList)
+    .then(response => {
+        response.forEach(rs => {
+            _.merge(responseObject, rs)
+        })
+        res.send(responseObject)
+    })
+}
+
+// async function getSteamFavStats (req, res, next) {
+//     let favList = req.body.appids
+//     delete req.body.appids
+//     let promiseList = favList.map(appid => {
+//         req.body.appid = appid
+//         services.steam.getUserStatsForGame(req.body)
+//     })
+// }
 
 async function shapeRequest (req, res, next) {
     let service = req.params.service_name.toLowerCase()
@@ -15,4 +43,4 @@ async function shapeRequest (req, res, next) {
     })
 }
 
-module.exports = {shapeRequest}
+module.exports = {shapeRequest, allUserInfo}
